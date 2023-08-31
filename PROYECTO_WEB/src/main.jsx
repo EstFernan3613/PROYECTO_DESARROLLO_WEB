798931683ae82b0be9441cd3d9befec882e0e575
Fernan3613 <juan_est.fernandez@uao.edu.co>
2023-08-31 14:34:43 -0500

    Carpetas Parte 2

--- PROYECTO_WEB/src/main.jsx
+++ PROYECTO_WEB/src/main.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import NavBar from '../../Components/Index/NavBar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <NavBar />
   </React.StrictMode>,
 )
